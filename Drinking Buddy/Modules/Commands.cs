82ab214ec75bb070736cdf413256ab4cfa95c6f1
Jamey0621 <you@example.com>
2020-12-04 18:21:03 -0600

    Started the project and added the meme api

---
+++ Drinking Buddy/Modules/Commands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Newtonsoft.Json.Linq;
+using Discord.WebSocket;
+
+
+namespace Drinking_Buddy.Modules
+{
+    class Commands
+    {
+        [Command("meme")]
+        public async Task Meme()
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("https://www.reddit.com/r/memes/random.json?limit=1");
+            JArray arr = JArray.Parse(result);
+            JObject post = JObject.Parse(arr[0]["data"]["children"][0]["data"].ToString());
+
+            var builder = new EmbedBuilder()
+                .WithImageUrl(post["url"].ToString())
+                .WithColor(new Color(33, 176, 252))
+                .WithTitle(post["title"].ToString())
+                .WithUrl("https://reddit.com" + post["permalink"].ToString());
+
+            var embed = builder.Build();
+            
+
+
+        }
+    }
+}
